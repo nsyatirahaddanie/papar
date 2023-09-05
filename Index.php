1652016476a512bcedef89f69dd1bd0013353e65
Aininsofi <aininsofiya312@gmail.com>
2023-09-05 16:49:03 +0800

    tambah

---
+++ Index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <style>
+            textarea {
+                resize: none;
+            }
+        </style>
+    </head>
+    <body>
+
+    <p><label for="text_area">cerita tentang diri saya:</label></p>
+
+    <textarea id="text_area" name="text_area" rows="7" cols="50">
+        Nama saya ainin sofiya . Umur saya 20 tahun . Saya berasal dari pulau pinang. Saya mengambil diploma dalam bidang teknologi maklumat dan komunikasi.
+    </textarea>
+
+    <br>
+    <input type="button" name="simpan" value="simpan">
+
+    </body>
+</html>
+
+
